a9cdcd592abc2a59fd6f0ebf1821a538ad207e1e
erik.slowikowski <erik.slowikowski@dreamit.de>
2018-07-04 15:28:27 +0200

    reworked server class more structure

--- src/chatti/Server.java
+++ src/chatti/Server.java
@@ -1,10 +1,13 @@
 package chatti;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Server {
 
@@ -12,30 +15,97 @@ public class Server {
     private static PrintWriter writer;
     private static BufferedReader reader;
 
-    public static void main(String[] args) throws IOException {
+    ServerSocket server;
+    ArrayList<PrintWriter> list_clientWriter;
 
-        ServerSocket serverSocket;
-        ExecutorService executor = Executors.newFixedThreadPool(30);
+    final int LEVEL_ERROR = 1;
+    final int LEVEL_NORMAL = 0;
 
-        try {
+    public static void main(String[] args) {
 
-            serverSocket = new ServerSocket(portNumber);
-            System.out.println("Server started");
+        Server s = new Server();
+        if(s.runServer()){
+            s.listenToClients();
+        } else {
+            //do nothing
+        }
+    }
 
-            while (true) {
+    public class ClientHandler implements Runnable {
 
-                try {
-                    Socket client = serverSocket.accept();
+        Socket client;
+        BufferedReader reader;
 
-                    executor.execute(new ClientThread(client));
+        public ClientHandler(Socket client) {
+            try {
+                this.client = client;
+                reader = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
 
+        @Override
+        public void run() {
+            String message;
+
+            try {
+                while ((message = reader.readLine()) != null) {
+                    appendTextToConsole("From Client: \n" + message, LEVEL_NORMAL);
+                    sendToAllClients(message);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+        public void listenToClients(){
+            while(true){
+                try{
+                    Socket client = server.accept();
+
+                    PrintWriter writer = new PrintWriter(client.getOutputStream());
+                    list_clientWriter.add(writer);
+
+                    Thread clientThread = new Thread(new ClientHandler(client));
+                    clientThread.start();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
-        }catch (IOException e1){
-            e1.printStackTrace();
         }
 
+        public boolean runServer(){
+            try{
+                server = new ServerSocket(portNumber);
+                appendTextToConsole("Server started on port "+portNumber, LEVEL_ERROR);
+
+                list_clientWriter = new ArrayList<PrintWriter>();
+                return true;
+            } catch (IOException e) {
+                appendTextToConsole("Server couldnt be started!", LEVEL_ERROR);
+                e.printStackTrace();
+                return false;
+            }
+        }
+
+        public void appendTextToConsole(String message, int level){
+            if(level == LEVEL_ERROR) {
+                System.err.println(message+"\n");
+            }
+            else {
+                System.out.println(message+"\n");
+            }
+        }
+
+        public void sendToAllClients(String message){
+            Iterator it = list_clientWriter.iterator();
+
+            while (it.hasNext()){
+                PrintWriter writer = (PrintWriter) it.next();
+                writer.print(message);
+                writer.flush();
+        }
     }
 }
